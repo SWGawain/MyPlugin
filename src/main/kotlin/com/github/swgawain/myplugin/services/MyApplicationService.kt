e7cc66b981f7efd93749a2f90ca86c2e4a842ded
GitHub Action <action@github.com>
2020-10-28 08:43:34 +0000

    Template cleanup

--- src/main/kotlin/com/github/swgawain/myplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/swgawain/myplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.swgawain.myplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.swgawain.myplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/swgawain/myplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/swgawain/myplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.swgawain.myplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.swgawain.myplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
